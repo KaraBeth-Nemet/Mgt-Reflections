e1a9f2085020c7f0e363e6e92b2868d5e6bdb52a
KaraBeth <43915118+KaraBeth-Nemet@users.noreply.github.com>
2024-09-08 19:24:35 +0000

    very (and i mean VERY) rough version of the idea of the website

---
+++ mgt-reflections-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatGridListModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'mgt-reflections';
+}
